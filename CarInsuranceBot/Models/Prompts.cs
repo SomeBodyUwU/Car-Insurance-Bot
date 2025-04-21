b6fcd5fc82db9c4e38745cb06da5a3ffae4357c6
SomeBodyUwU <dima@zosia.kiev.ua>
2025-04-21 15:46:08 +0300

    minor prompts changes

--- CarInsuranceBot/Models/Prompts.cs
+++ CarInsuranceBot/Models/Prompts.cs
@@ -20,7 +20,7 @@ namespace CarInsuranceBot.Models
             public const string dataConfirmationDeny = "User disagreed with the extracted data. Tell tell user to send again a clear photo of his *passport* 📷.";
             public const string dataConfirmationAgree = "User agreed with the extracted data. Inform the user that the fixed price for the insurance is 100 USD. Ask the user if they agree with the price.";
             public const string priceConfirmation = "Please tell user to type or to use reply-keyboard to say 'yes' or 'no' whether user is agree or disagree with fixed 100 USD price.";
-            public const string priceConfirmationDeny = "Used disagreed with the fixed 100 USD insurance price. Apologize and explain that 100 USD is the only available price.";
+            public const string priceConfirmationDeny = "Used disagreed with the fixed 100 USD insurance price. Apologize and explain that 100 USD is the only available price. Ask again if user agrees with the price.";
             public const string priceConfirmationAgree = "Used agreed with the fixed 100 USD insurance price.";
         }
     }
